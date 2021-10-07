1fca5da79ddbae7283af527fb698a4c42c4f9047
Piotr Mec <it@ectg.pl>
2021-10-07 11:25:18 +0200

    cart tests

--- test/test1-chromium.js
+++ test/test1-chromium.js
@@ -1,13 +1,16 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const { expect } = require('chai');
+const { Options } = require('selenium-webdriver/chrome');
 
 /**
- * this prestashop is slow!!! breaks without sleeps, sorry
+ * this prestashop is slow!!! breaks without sleeps, sorry. 
  */
 describe('HomepageTest', () => {
     // TODO: browserstack?
-    const driver = new Builder().forBrowser('chrome').build();
-    const sleepBetweenSteps = 4000;
+    // const driver = new Builder().forBrowser('firefox').build();
+    const driver = new Builder().usingServer().withCapabilities({ 'browserName': 'chrome' }).build();
+    const sleepBetweenSteps = 3000;
+
 
     it('automationpractice.com should display e-commerce site My Store', async () => {
         await driver.get('http://automationpractice.com/');
@@ -18,13 +21,13 @@ describe('HomepageTest', () => {
 
     it('should add product to cart from dashboard', async () => {
         const button = await driver.findElement(
-            By.css('.ajax_block_product .right-block .button-container a.ajax_add_to_cart_button')
+            By.css('#homefeatured > .ajax_block_product:nth-child(1) .button:nth-child(1) > span')
         )
-        button.click();
+        // buttons are hidden - .click() won't work with many drivers
+        await driver.executeScript("arguments[0].click();", button)
         await driver.sleep(sleepBetweenSteps);
         const closeButton = await driver.findElement(By.css('.layer_cart_product .cross'))
         closeButton.click()
-        await driver.sleep(sleepBetweenSteps);
         const cartQuantity = await driver.findElement(By.css('.ajax_cart_quantity')).getAttribute("innerHTML");
         expect(cartQuantity).to.equal('1');
     })
@@ -33,21 +36,31 @@ describe('HomepageTest', () => {
         const button = await driver.findElement(
             By.css('.ajax_block_product:nth-child(2) .right-block .button-container a.ajax_add_to_cart_button')
         )
-        button.click();
+        await driver.executeScript("arguments[0].click();", button)
         await driver.sleep(sleepBetweenSteps);
         const closeButton = await driver.findElement(By.css('.layer_cart_product .cross'))
         closeButton.click()
-        await driver.sleep(sleepBetweenSteps);
         const cartQuantity = await driver.findElement(By.css('.ajax_cart_quantity')).getAttribute("innerHTML");
         expect(cartQuantity).to.equal('2');
     })
 
+    it('add to cart should work on product page', async () => {
+        await driver.get('http://automationpractice.com/index.php?id_product=1&controller=product')
+        const cartButton = await driver.findElement(By.css('#add_to_cart button'));
+        await driver.executeScript("arguments[0].click();", cartButton)
+        await driver.sleep(sleepBetweenSteps);
+        const closeButton = await driver.findElement(By.css('.layer_cart_product .cross'))
+        closeButton.click()
+        const cartQuantity = await driver.findElement(By.css('.ajax_cart_quantity')).getAttribute("innerHTML");
+        expect(cartQuantity).to.equal('3');
+    })
+
     it('cart should persist when navigating to cart page', async () => {
         const cartButton = await driver.findElement(By.css('.shopping_cart a'));
-        cartButton.click()
+        await driver.executeScript("arguments[0].click();", cartButton)
         await driver.sleep(sleepBetweenSteps);
         const cartLabel = await driver.findElement(By.id('summary_products_quantity')).getAttribute('innerHTML');
-        expect(cartLabel).to.equal('2 Products');
+        expect(cartLabel).to.equal('3 Products');
     })
 
     after(async () => driver.quit());
